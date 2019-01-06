2cc64087bdfdcc624351d5dc31fd69678b9e26a0
zhaokaikai <2376254038@qq.com>
2019-01-06 23:38:30 +0800

    selenium自动化

---
+++ selenium.js
@@ -0,0 +1,23 @@
+require('chromedriver'); //chrome浏览器驱动
+let webdriver = require('selenium-webdriver'); //浏览器自动操作
+let cheerio = require('cheerio'); //获取页面数据
+let fs = require('fs');
+
+let driver = new webdriver.Builder().forBrowser('chrome').build()
+var url = 'https://search.jd.com/Search?keyword=u%E7%9B%98&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&suggest=1.def.0.V13&wq=upan&psort=3&page=3&s=61&click=0'
+if(window.scroll)
+async function start() {
+    await driver.get(url);
+    await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)')
+    driver.getPageSource().then((val) => {
+        const $ = cheerio.load(val);
+        let result = $('#u1').text()
+        console.log(result)
+        console.log(`li的长度：${$('.gl-warp li').length}`)
+        fs.writeFile('./mes.txt', result, (err) => {
+            if (err) throw err
+            console.log('is saved')
+        })//将数据写进mes.txt文件中
+    })
+}
+start()
