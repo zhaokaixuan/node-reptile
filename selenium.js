399a049c741aac881c35d54bcd2fe171c3df0970
zhaokaikai <2376254038@qq.com>
2019-01-06 23:50:17 +0800

    添加等待时间wait

--- selenium.js
+++ selenium.js
@@ -5,10 +5,10 @@ let fs = require('fs');
 
 let driver = new webdriver.Builder().forBrowser('chrome').build()
 var url = 'https://search.jd.com/Search?keyword=u%E7%9B%98&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&suggest=1.def.0.V13&wq=upan&psort=3&page=3&s=61&click=0'
-if(window.scroll)
 async function start() {
     await driver.get(url);
-    await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)')
+    await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+    await driver.sleep(2000);
     driver.getPageSource().then((val) => {
         const $ = cheerio.load(val);
         let result = $('#u1').text()
