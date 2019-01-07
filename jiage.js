9495b5782cb731edd630d52c3d5cc01e533b12a4
zhaokaikai <2376254038@qq.com>
2019-01-08 00:09:28 +0800

    价格信息

--- jiage.js
+++ jiage.js
@@ -3,10 +3,13 @@ var fs = require('fs');
 var iconv = require('iconv-lite');
 var arr = JSON.parse(fs.readFileSync('sku.json'));
 var newArr = arr.map((item)=>{
-    return item.join(',');
+    var a = item.map((val)=>{
+        return 'J_'+val;
+    });
+    return a.join('%2C');
 })
 var resultArr = []
-var api = `https://p.3.cn/prices/mgets?callback=jQuery9207438&skuIds=J_1337239%2CJ_631987`
+var api = `https://p.3.cn/prices/mgets?skuIds=`
 newArr.map((item)=>{
     var url = api + item
     request({
@@ -21,7 +24,7 @@ newArr.map((item)=>{
             resultArr.push(obj);
             console.log(`数组长度：${resultArr.length}`)
             if(resultArr.length == 100){
-                fs.writeFile('./pingjia.json',JSON.stringify(resultArr),function(err){
+                fs.writeFile('./jiage.json',JSON.stringify(resultArr),function(err){
                     if (err) throw err;
                     console.log('写入成功')
                 })
