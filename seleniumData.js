1bb8e14d09b75475c0a62c56adb422a627ad1d77
zybeargithub <1106408264@qq.com>
2019-01-07 16:39:36 +0800

    递归进行爬取页面

--- seleniumData.js
+++ seleniumData.js
@@ -2,26 +2,42 @@ require('chromedriver'); //chrome浏览器驱动
 let webdriver = require('selenium-webdriver'); //浏览器自动操作
 let cheerio = require('cheerio'); //获取页面数据
 let fs = require('fs');
+let async = require('async');
 let driver = new webdriver.Builder().forBrowser('chrome').build()
-var url = 'https://item.jd.com/'
-var sku = '6268372';
-async function start(sku) {
-    await driver.get(url+sku+'.html');
+var rootUrl = 'https://item.jd.com/';
+var skuArr = JSON.parse(fs.readFileSync('sku.json').toString());
+var urlArr = skuArr.map((item)=>{
+    return {url:rootUrl + item + '.html',sku:item};
+})
+
+var count = 0;
+var arr = [];
+async function start(url,sku) {
+    await driver.get(url);
     await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
     await driver.sleep(2000);
     driver.getPageSource().then((val) => {
         const $ = cheerio.load(val);
-        console.log($('.contact').find('.u-jd').text())
         var isJd = $('.contact').find('.u-jd').text();
         var shopName = $('.J-hove-wrap item').eq(0).find('a').text();
         var price = $('.summary-price').find('.p-price').text();
         var obj = {
+            sku:sku,
             isJd: isJd,
             shopName: shopName,
             price: price
         }
         console.log(obj)
-        driver.close()
+        arr.push(obj);
+        console.log(`第${count}个页面；数据数：${arr.length};url:${url}`)
+        count++;
+        if(count<urlArr.length){
+            start(urlArr[count].url,urlArr[count].sku);
+        }else{
+            console.log(arr);
+            driver.close()
+        }
+
         // fs.writeFile('./sku.json', JSON.stringify(titles), (err) => {
         //     if (err) throw err
         //     console.log('is saved')
@@ -29,4 +45,4 @@ async function start(sku) {
         // })
     })
 }
-start(sku)
+
